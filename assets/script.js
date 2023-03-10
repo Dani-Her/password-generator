815376bb716f530868674947ef40c47bd27f55b5
Daniela Herteg <dotherteg1325@gmail.com>
2023-03-10 10:41:05 +0000

    functions added

--- assets/script.js
+++ assets/script.js
@@ -90,17 +90,98 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  let allCharacters = [];
+  let endResult = [];
+  let validatorCounter = 0;
 
+  charLength = prompt("How long would you like the password to be?");
+
+  if (charLength > 7 && charLength < 129) {
+  } else {
+    return "Please choose password length between 8 and 128 digits!";
+  }
+
+lowerChar = confirm("Any lower case characters?");
+
+if (lowerChar) {
+  allCharacters = allCharacters.concat(lowerChar);
+  var rand = getRandoInteg(lowerChar.length);
+  endResult.push(lowerChar[rand]);
+  validatorCounter++;
 }
 
+upperCase = confirm(
+  "Any upper case characters?"
+);
+if (upperCase) {
+  allCharacters = allCharacters.concat(upperCase);
+  var rand = getRandoInteg(upperCase.length);
+  endResult.push(upperCase[rand]);
+  validatorCounter++;
+}
+
+numbers = confirm(
+  "Any numbers?"
+);
+if (numbers) {
+  allCharacters = allCharacters.concat(numbers);
+  var rand = getRandoInteg(numbers.length);
+  endResult.push(numbers[rand]);
+  validatorCounter++;
+}
+
+specialChar = confirm(
+  "Any special characters?"
+);
+if (specialCharacters) {
+  allCharacters = allCharacters.concat(specialCharacters);
+  var rand = getRandoInteg(specialCharacters.length);
+  endResult.push(specialCharacters[rand]);
+  validatorCounter++;
+}
+
+if (
+  lowerChar === false &&
+  upperCase === false &&
+  numbers === false &&
+  specialChar === false
+) {
+  return "Please be sure to choose at least one option! \nPress the button to try again!";
+}
+
+// Final Password 
+for (i = 0 + validatorCounter; i < allCharacters.length; i++) {
+  var rand = getRandoInteg(allCharacters.length);
+  endResult.push(allCharacters[rand]);
+}
+
+shuffle(endResult);
+return endResult.join("");
+}
 // Function for getting a random element from an array
-function getRandom(arr) {
+function shuffle(array) {
+  var d = array.length, 
+  a,
+  n;
+  while (d) {
+    n = Math.floor(Math.random() * d --);
+    a = array[d];
+    array[d] = array[n];
+    array[n] = a;
+  }
 
+  return array;
 }
 
+function getRandoInteg(max) {
+  return Math.floor(Math.random()*Math.floor(max));
+}
 // Function to generate password with user input
-function generatePassword() {
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
 }
 
 // Get references to the #generate element
@@ -108,7 +189,7 @@ var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = getPasswordOptions();
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
